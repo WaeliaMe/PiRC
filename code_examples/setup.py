105da44887f160d5340dc3ea509c27fd5c3dd399
WaeliaM <waelballouk31@gmail.com>
2026-03-17 20:26:38 +0100

    Add setup.py for Pi Hybrid examples package

---
+++ code_examples/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pi_hybrid_examples",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Example code for Pi Hybrid Token Model - token allocation and reserve redistribution",
+    author="Wael Ballouk",
+    python_requires='>=3.7',
+)
